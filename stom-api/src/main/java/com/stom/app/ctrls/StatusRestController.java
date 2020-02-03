e2fd57d0b5bfc00e505ab8d45cfcaa7555dffcde
djordjeprzulj <przulj@gmail.com>
2020-02-03 18:11:48 +0100

    status rest modifikovan

--- stom-api/src/main/java/com/stom/app/ctrls/StatusRestController.java
+++ stom-api/src/main/java/com/stom/app/ctrls/StatusRestController.java
@@ -6,47 +6,38 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.stom.app.jpa.Status;
 import com.stom.app.reps.StatusRepository;
 
 @RestController
 @CrossOrigin
+@RequestMapping("status")
 public class StatusRestController {
 	
 	@Autowired
 	private StatusRepository statusRepository;
 	
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-	
-	@RequestMapping(value = "statusNextId", method = RequestMethod.GET)
-	public int getNextId() {		
-        Integer sledeci = jdbcTemplate.queryForObject("SELECT max(id)+1 "
-													+ "FROM status", Integer.class);
-        if (sledeci == null)
-        	sledeci = new Integer(1);
-        return sledeci.intValue();
-	}
-	
-	@RequestMapping(value = "status", method = RequestMethod.GET)
+	@GetMapping
 	public Collection<Status> getAll() {
 		return statusRepository.findAll();
 	}
 	
-	@RequestMapping(value = "status/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/{id}")
     public ResponseEntity<Status> get(@PathVariable("id") int id) {
         Optional<Status> obj = statusRepository.findById(id);
         return new ResponseEntity(obj, HttpStatus.OK);
     }
 		
-	@RequestMapping(value = "status", method = RequestMethod.POST)
+	@PostMapping
     public ResponseEntity<Void> insertStatus(@RequestBody Status obj) {
         if (statusRepository.existsById(obj.getId())) {
             return new ResponseEntity<Void>(HttpStatus.CONFLICT);
@@ -55,7 +46,7 @@ public class StatusRestController {
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 	
-	@RequestMapping(value = "status/{id}", method = RequestMethod.PUT)
+	@PutMapping(value = "/{id}")
     public ResponseEntity<Void> updateStatus(@PathVariable("id") Integer id, @RequestBody Status obj) {
         if (!statusRepository.existsById(id)) {
             return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
@@ -64,7 +55,7 @@ public class StatusRestController {
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 	
-	@RequestMapping(value = "status/{id}", method = RequestMethod.DELETE)
+	@DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deleteStatus(@PathVariable("id") Integer id) {
         if (!statusRepository.existsById(id)) {
             return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
