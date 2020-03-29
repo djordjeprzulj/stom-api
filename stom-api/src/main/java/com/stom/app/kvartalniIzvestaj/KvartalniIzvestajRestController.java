c2fa5c016fcf8e1175ccddc300b637591f849f31
marko <marko.stojanovic19@gmail.com>
2020-03-29 19:56:58 +0200

    Implementirano generisanje pdf fajla za KvartalniIzvestaj

--- stom-api/src/main/java/com/stom/app/kvartalniIzvestaj/KvartalniIzvestajRestController.java
+++ stom-api/src/main/java/com/stom/app/kvartalniIzvestaj/KvartalniIzvestajRestController.java
@@ -1,24 +1,16 @@
 package com.stom.app.kvartalniIzvestaj;
 
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.io.FileNotFoundException;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import net.sf.jasperreports.engine.JRException;
 
 @RestController
 @CrossOrigin
@@ -27,13 +19,25 @@ public class KvartalniIzvestajRestController {
 	@Autowired
 	private KvartalniIzvestajInterface kvartalniIzvestajInterface;
 	
+	KvartalniIzvestaj kvartalniIzvestaj;
+	
 	
 	@GetMapping(value = "kvartalniIzvestaj/{pocIntervala}/{krajIntervala}")
 	public ResponseEntity<KvartalniIzvestaj> getData(@PathVariable("pocIntervala") String _pocIntervala, @PathVariable("krajIntervala") String _krajIntervala) throws SQLException{
 		
-		KvartalniIzvestaj kvartalniIzvestaj = kvartalniIzvestajInterface.generisiKvartalniIzvestaj(_pocIntervala,_krajIntervala);
+		kvartalniIzvestaj = kvartalniIzvestajInterface.generisiKvartalniIzvestaj(_pocIntervala,_krajIntervala);
 		
 		return new ResponseEntity<KvartalniIzvestaj>(kvartalniIzvestaj, HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "kvartalniIzvestaj/pdfReport")
+	public ResponseEntity<String> getPdfReport() throws FileNotFoundException, JRException{
+		
+		String pdfDestination = kvartalniIzvestajInterface.generatePDF(kvartalniIzvestaj);
+		
+		return new ResponseEntity<String>(pdfDestination, HttpStatus.OK);
+		
+		
 	}
 
 }
